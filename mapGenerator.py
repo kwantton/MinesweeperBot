1bea3fe9ec21af8565a264687b27a5e3a0c0847e
Antton Kasslin <antton.kasslin@hotmail.com>
2024-09-10 02:08:05 +0300

    pygame: (1) If you hit a mine, it's shown on the map as a mine like in actual minesweeper (2) If you hit a zero, all the adjacent non-mine cells are opened like in actual minesweeper This is also useful for the actual bot logic, as it too needs to have a list of which cells have already been opened

--- mapGenerator.py
+++ mapGenerator.py
@@ -7,6 +7,7 @@ class Minesweeper:
     def __init__(self, width, height, mines):
         pygame.init()
         self.mouse_pos = (0,0)
+        self.opened = set()      # all seen zero-cells are gathered here {(x0,y0), (x1,y1),...}
 
         self.scale = 50         # how many px in height and width should each cell (image) be?
         self.height = height    # map height measured in in rows
@@ -55,7 +56,7 @@ class Minesweeper:
             print('mouse tracker;')
             print('- x//self.scale, y//self.scale:', x//self.scale, y//self.scale)
             self.mouse_pos = mouse_pos
-
+    
     def handle_first_click(self, mouse_x, mouse_y):
         print("\nhandle_first_click();")
         self.started = True
@@ -63,11 +64,36 @@ class Minesweeper:
         # based on which cell was clicked, place the mines (do not place a mine in the cell that was just clicked)
         self.available_coordinates = [(x,y) for y in range(self.height) for x in range(self.width) if (x,y) != (mouse_x, mouse_y)]
         self.mine_locations = set(sample(self.available_coordinates,self.mines))
-        print(f'- klikattiin koordinaatteja {mouse_x, mouse_y} ja miinat laitettiin seuraavasti:\n', self.mine_locations)
+        print(f'- clicked coordinates {mouse_x, mouse_y} and placed the mines as follows:\n', self.mine_locations)
+        self.handle_click(mouse_x, mouse_y)
 
     def handle_click(self, mouse_x, mouse_y):
         print('\nhandle_click();')
-        print('- mouse_x, mouse_y:', (mouse_x, mouse_y))  # HUOM! (15,0) on x-painike c:
+        if (mouse_x, mouse_y) in self.opened:
+            print('- WAS OPEN ALREADY')
+            return
+        self.opened.add((mouse_x, mouse_y))                 # why: in case a zero is clicked open, I'm using handle_click recursively to open up all the surrounding cells that are not mines. For that, this list is needed, so that an endless recursion doesn't occur.
+        print('- mouse_x, mouse_y:', (mouse_x, mouse_y))
+        if (mouse_x, mouse_y) in self.mine_locations:
+            print('- HIT A MINE AT COORDINATES:', (mouse_x, mouse_y))
+            self.map[mouse_y][mouse_x] = 'images/mine.png'
+        else:
+            neighbours = self.surrounding_cells_coordinates(mouse_x, mouse_y)  # finds all the actual surrounding cells
+            label = 0
+            for neighbour in neighbours:
+                if neighbour in self.mine_locations:
+                    label += 1
+            self.map[mouse_y][mouse_x] = f'images/{label}.png'
+            if label == 0:
+                for neighbour in neighbours:
+                    self.handle_click(neighbour[0], neighbour[1])
+                # TO-DO: POISTA? self.handle_adjacent_zeros(mouse_x, mouse_y, neighbours)        # So, this clicked cell was '0', therefore  in case an adjacent zero is found, all the zeros next to it should also be opened, and all the cells next to all the zeros as well!
+
+    def surrounding_cells_coordinates(self, x, y):
+        candidates = [(w,h) for h in range(y-1, y+1+1) for w in range(x-1,x+1+1) if 0<=w<self.width and 0<=h<self.height]
+        candidates.remove((x,y))
+        return candidates
+        # print('\nsourrounding_cells_coordinates():', candidates)
 
     def draw_display(self):                     # for the matrix, draw the correct image for each cell
         self.screen.fill((0,0,0))
@@ -89,4 +115,4 @@ if __name__ == '__main__':
     beginner = 9,9,10
     intermediate = 16,16,40
     expert = 30,16,99           # width, height, mines
-    Minesweeper(2,1,1)
+    Minesweeper(3,3,1)
