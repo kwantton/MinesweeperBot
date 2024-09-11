0a53f094fdb45ba7b6145dce1bc1122908b32387
Antton Kasslin <antton.kasslin@hotmail.com>
2024-09-11 16:27:03 +0300

    (1) Created 'botGame.py' that was initially a copy of the 'mapGenerator.py' which in effect was a human-playable minesweeper. This 'botGame.py' is meant to enable a human to play the game AND the bot to play the game (2) Quite a bit of clean-up of the 'botGame.py' so that it's easier to develop the bot (3) Initialized variables for the bot, starting to build the bot now

--- botGame.py
+++ botGame.py
@@ -1,20 +1,21 @@
 import pygame
 from random import sample
+from constraint import Problem
+from label_names import flag, unclicked, mine, safe, name
 
-# cell = a square of the map
-
+# cell = a clickable square of the minesweeper map, 'ruutu'
 class Minesweeper:
     def __init__(self, width, height, mines):
         pygame.init()
         pygame.display.set_caption('MINESWEEPER')
         self.mouse_pos = (0,0)
-        self.opened = set()     # all seen zero-cells are gathered here {(x0,y0), (x1,y1),...}
-        self.to_open = width*height - mines
+        self.opened = set()         # all opened cells are gathered here {(x0,y0), (x1,y1),...}
+        self.cells_to_open = width*height - mines
 
-        self.scale = 50         # how many px in height and width should each cell be?
-        self.infobar = 100      # pixels above the actual map
+        self.scale = 50             # how many px in height and width should each cell be?
+        self.infobar = 100          # pixels above the actual map
         self.font = pygame.font.Font(None, 36)
-        self.height = height    # map height measured in in rows
+        self.height = height        # map height measured in in rows
         self.width = width
 
         self.mines = mines
@@ -33,123 +34,137 @@ class Minesweeper:
         print('\nload_images')
         image_names = ['images/' + name + '.png' for name in '0 1 2 3 4 5 6 7 8 flag mine unclicked has_to_have_a_mine safe'.split()]
         for image_name in image_names:
-            self.images[image_name]=pygame.transform.scale(pygame.image.load(image_name), (self.scale,self.scale))
+            self.images[image_name] = pygame.transform.scale(pygame.image.load(image_name), (self.scale,self.scale))
 
-    def new_game(self):
+    def new_game(self):     # initialize variables
         print("\nnew_game")
-        self.started = False                                                        # the mines will be placed AFTER the first click, as in real minesweeper. Otherwise you could lose on the first click. For that, we need to keep track on if the first click has already commenced or not.
-        self.victory = False
-        self.hit_a_mine = False
-        self.minecount = self.mines
+        self.bot_x = 0
+        self.bot_y = 0
+        self.front = set()                  # cells that are not finished
+        self.inner = set()                  # cells that are not finished and are not neighboured by any opened cells (i.e., are not 'seen' by any opened cells)
         self.opened = set()
-        self.start_time = None
-        self.timer_active = False
+        self.started = False                # the mines will be placed AFTER the first click, as in real minesweeper. Otherwise you could lose on the first click. For that, we need to keep track on if the first click has already commenced or not.
+        self.victory = False
+        self.finished = set()               # cells where label is 0 OR the number of surrounding mines = label
         self.elapsed_time = 0
-        self.map = [['images/unclicked.png' for x in range(self.width)] for y in range(self.infobar, self.height+self.infobar)]   # the visuals; names of the images of each cell!
+        self.start_time = None
+        self.hit_a_mine = False
+        self.timer_active = False
+        self.minecount = self.mines
+        self.map = [[unclicked for x in range(self.width)] for y in range(self.infobar, self.height+self.infobar)]   # the visuals; names of the images of each cell!
+        # self.problem = Problem()    # 'constraint satisfaction problem' (CSP) solver class (https://pypi.org/project/python-constraint/). I'm using 'problem.addVariables(list_of_cells, [domain_member_1, domain_member_2,...])' where 'domain' = lähtöjoukko, i.e. the constraints, i.e. the list of accepted values for each variable, i.e. {0,1} in the case of minesweeper. This helps narrowing down the results (I do not know how it's implemented in the class, though!)
 
     def inspect_event(self, event):
-        #self.mouse_tracker()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:                                         # event.key, not event.type, sigh. I was looking for this with cats and dogs
                 self.new_game()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.key == pygame.K_RETURN:                                      # BOT:
+                self.bot_brain()                                                    # the bot makes a move when you press enter
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             print(f'inspect_event(); MOUSEBUTTONDOWN;')
             mouse_x, mouse_y = pygame.mouse.get_pos()                               # to-do: make a check if it's on a cell, if needed!
             mouse_x //= self.scale
             mouse_y = (mouse_y - self.infobar)//self.scale                          # adjust for the infobar ('raise' the click by infobar height), then get the row number by division by self.scale. Asked from ChatGPT when trying to find the problem
             if mouse_y < 0:
                 self.new_game()                                                     # if you click the top bar, it starts a new game
-            elif event.button == 1:                                                   # left click == 2!
+            elif event.button == 1:                                                 # left click == 2!
                 print(f'- mouse_x//scale, mouse_y//scale: {mouse_x, mouse_y}')      # if each cell width is e.g. 100 px, then if you click e.g. on x-coord 540, it's the 6th column (40 would be 1st)
                 if not self.started:
                     self.handle_first_left_click(mouse_x, mouse_y)
                 else:
-                    self.handle_left_click(mouse_x, mouse_y)
+                    self.probe(mouse_x, mouse_y)
             elif event.button == 3:                                                 # right click == 3!
                 self.handle_right_click(mouse_x, mouse_y)
-        if event.type == pygame.QUIT:
+        elif event.type == pygame.QUIT:
             exit()
-
-    def mouse_tracker(self):
-        mouse_pos = pygame.mouse.get_pos()
-        if mouse_pos != self.mouse_pos:
-            x, y = mouse_pos
-            print('mouse tracker;')
-            print('- x//self.scale, y//self.scale:', x//self.scale, y//self.scale)
-            self.mouse_pos = mouse_pos
     
     def handle_first_left_click(self, mouse_x, mouse_y):
+        self.bot_x = mouse_x                                                        # the bot starts where you first click
+        self.bot_y = mouse_y
         print("\nhandle_first_left_click();")
         self.started = True
         self.start_time = pygame.time.get_ticks()
         self.timer_active = True
+        self.generate_map(mouse_x, mouse_y)
+        self.probe(mouse_x, mouse_y)
 
-        # based on which cell was clicked, place the mines (do not place a mine in the cell that was just clicked)
-        self.available_coordinates = [(x,y) for y in range(self.height) for x in range(self.width) if (x,y) != (mouse_x, mouse_y)]
-        self.mine_locations = set(sample(self.available_coordinates,self.mines))
+    # based on the coordinates of the first clicked cell (mouse_x, mouse_y), place the mines elsewhere
+    def generate_map(self, mouse_x, mouse_y):
+        available_coordinates = [(x,y) for y in range(self.height) for x in range(self.width) if (x,y) != (mouse_x, mouse_y)]
+        self.mine_locations = set(sample(available_coordinates, self.mines))   # NB! This is the very line of code that actually generates the map by registering all mine locations!! This samples 'self.mines' number of mines (e.g. 99 in an expert game) from 'available_cordinates' which excludes the opening cell that was clicked.
         print(f'- clicked coordinates {mouse_x, mouse_y} and placed the mines as follows:\n', self.mine_locations)
-        self.handle_left_click(mouse_x, mouse_y)
 
-    def handle_left_click(self, x, y):
-        print('\nhandle_click();')
-        print('- mouse_x, mouse_y:', (x, y))
-        neighbours = self.neighbours_coordinates(x, y)  # finds all the actual surrounding cells
+    def probe(self, x, y):
+        print('\nprobe();')
+        print('- entered cell (x,y):', (x, y))
 
         if (x, y) in self.mine_locations:
-            print('- HIT A MINE AT COORDINATES:', (x, y))
-            self.map[y][x] = 'images/mine.png'
-            self.hit_a_mine = True
-            self.timer_active = False
-            self.draw_display()
+            self.map[y][x] = mine
+            self.game_over(x,y)
             return
-        elif self.map[y][x] == 'images/flag.png':   # if you left click on a flag, it does nothing
-            return
+        elif self.map[y][x] == flag:           # if you left click on, i.e. 'probe', a red flag, it does nothing (like in real minesweeper)
+            pass
         elif (x, y) in self.opened:
             print('- OPEN ALREADY')
-            
+            neighbours = self.neighbours_coordinates(x, y)  # finds all the actual cells neighbouring (x,y)
+
             flag_count = self.count_surrounding_flags(neighbours)
             label = self.map[y][x]
-            if f'images/{flag_count}.png' == label:
+            if name(flag_count) == label:
                 self.handle_chord(x, y)
             return
         else:
+            neighbours = self.neighbours_coordinates(x, y)
             self.opened.add((x, y))                 # why: in case a zero is clicked open, I'm using handle_click recursively to open up all the surrounding cells that are not mines. For that, this list is needed, so that an endless recursion doesn't occur.
             
             label = 0
             for neighbour in neighbours:
                 if neighbour in self.mine_locations:
                     label += 1
-            self.map[y][x] = f'images/{label}.png'
+            self.map[y][x] = name(label)
             if label == 0:
                 for neighbour in neighbours:
-                    self.handle_left_click(neighbour[0], neighbour[1])
-                # TO-DO: POISTA? self.handle_adjacent_zeros(mouse_x, mouse_y, neighbours)        # So, this clicked cell was '0', therefore  in case an adjacent zero is found, all the zeros next to it should also be opened, and all the cells next to all the zeros as well!
+                    self.probe(neighbour[0], neighbour[1]) # i.e., open up all the neighbours!
         
+    def game_over(self, x, y):
+        print('- HIT A MINE AT COORDINATES:', (x, y))
+        self.hit_a_mine = True
+        self.timer_active = False
+        self.draw_display()            
+    
     def neighbours_coordinates(self, x, y):
         neighbours = [(w,h) for h in range(y-1, y+1+1) for w in range(x-1,x+1+1) if 0<=w<self.width and 0<=h<self.height and (w,h) != (x,y)]
         return neighbours
-        # print('\nsourrounding_cells_coordinates():', neighbours)
 
     def count_surrounding_flags(self, neighbours):
         count = 0
         for n in neighbours:
-            if self.map[n[1]][n[0]] == 'images/flag.png':
+            if self.map[n[1]][n[0]] == flag:
                 count += 1
         return count
     
     def handle_chord(self, x, y):
         for neighbour in self.neighbours_coordinates(x,y):
-            if self.map[neighbour[1]][neighbour[0]] == 'images/unclicked.png':  # without this, it would chord also flagged cells
-                self.handle_left_click(neighbour[0], neighbour[1])
+            if self.map[neighbour[1]][neighbour[0]] == unclicked:  # without this, it would chord also flagged cells
+                self.probe(neighbour[0], neighbour[1])
 
     def handle_right_click(self, x, y):
-        if self.map[y][x] == 'images/flag.png':
-            self.map[y][x] = 'images/unclicked.png'
+        if self.map[y][x] == flag:
+            self.map[y][x] = unclicked
             self.minecount += 1
-        elif self.map[y][x] == 'images/unclicked.png':
-            self.map[y][x] = 'images/flag.png'
+        elif self.map[y][x] == unclicked:
+            self.map[y][x] = flag
             self.minecount -= 1
 
+    def bot_brain(self):
+        print('\nhello from bot_brain!')
+        current_label = self.map[self.bot_y][self.bot_x]
+        print('- current_label:', current_label)
+        neighbours = self.neighbours_coordinates(self.bot_x, self.bot_y)        # self.bot_x and self.bot_y had been initially set in self.handle_first_left_click as the x (column number) and y (row number) of the first click
+        if name(len(neighbours)) == self.map[self.bot_y][self.bot_x]: # if the number of neighbours equals to the number of the current tile the flag all neighbours
+            for neighbour in neighbours:
+                self.map[neighbour[1]][neighbour[0]] = flag
+    
     def draw_display(self):
         self.screen.fill((0,0,0))
         minecount_surface = self.font.render(f'Mines left: {self.minecount}', True, (255,255,255))    # True is for antialias, white is 255
@@ -175,7 +190,7 @@ class Minesweeper:
         self.clock.tick(30)
     
     def map_cleared_check(self):
-        if len(self.opened) == self.to_open:
+        if len(self.opened) == self.cells_to_open:
             self.victory = True
             self.timer_active = False
     
---
+++ label_names.py
@@ -0,0 +1,8 @@
+# naming variables for convenience
+def name(label):
+    return f'images/{label}.png'
+
+flag    = name('flag')
+mine    = name('mine')
+unclicked  = name('unclicked')
+safe    = name('safe')
